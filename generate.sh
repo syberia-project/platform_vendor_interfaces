6bcb5331a95855bccaf4805ef8af934e71b64766
nato6613 <normandija1945@gmail.com>
2021-06-28 07:18:18 +0000

    generate.sh: fix error  find: ‘vendor/interfaces’: No such file or directory
    Change-Id: I9bb8332fc28e5b3b397334a141440cbfadee09a3

--- generate.sh
+++ generate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-find vendor/interfaces -type d -name '[0-9].[0-9]' |while read intf;do
+find $ANDROID_BUILD_TOP/vendor/interfaces -type d -name '[0-9].[0-9]' |while read intf;do
 	interface="$(echo $intf |sed -E 's;.*vendor/interfaces/(.*)/([0-9.]*).*;\1;g' | tr '/' '.')"
 	version="$(  echo $intf |sed -E 's;.*vendor/interfaces/(.*)/([0-9.]*).*;\2;g')"
 	full="${interface}@${version}"
